f8a97ee659b2e79d5e8c344b36160c3f9db80f57
Anton Potemkin <79133891911@yandex.ru>
2019-04-16 23:26:40 +0700

    fix computation

--- src/main/scala/lectures/functions/Computations.scala
+++ src/main/scala/lectures/functions/Computations.scala
@@ -13,7 +13,7 @@ trait Data {
   val dataArray = "Клара Цеткин обожала Карла Маркса".split(" ")
 }
 
-object Computation extends App with Data {
+object Computation extends Data {
 
   def computation(filterData: String, dataProducer: Array[String]): Array[String] = {
     //EMULATE HEAVY LOAD
@@ -39,7 +39,7 @@ object Computation extends App with Data {
   *
   * Какой тип имеет partiallyAppliedCurriedFunction - ?
   */
-object CurriedComputation extends App with Data {
+object CurriedComputation extends Data {
 
   def curriedComputation(filterData: String)(dataProducer: Array[String]): Array[String] = {
     //EMULATE HEAVY LOAD
@@ -64,18 +64,18 @@ object CurriedComputation extends App with Data {
   *
   * При этом постарайтесь минимизировать количество разбиений строки filterData на отдельные слова.
   */
-object FunctionalComputation extends App with Data {
+object FunctionalComputation extends Data {
 
-  def functionalComputation(filterData: String): Array[String] => Array[String] = (dataProducer: Array[String]) => {
-    //EMULATE HEAVY LOAD
-    Thread.sleep(10)
-    //PRODUCE WORDS ARRAY FROM A STRING
-    val filterArray = filterData.split(" ")
+  def functionalComputation(filterData: String): Array[String] => Array[String] = {
+      //EMULATE HEAVY LOAD
+      Thread.sleep(10)
+      //PRODUCE WORDS ARRAY FROM A STRING
+      val filterArray = filterData.split(" ")
 
     //EMULATE HEAVY LOAD
     Thread.sleep(100)
     // LEAVE ONLY EQUAL WORDS IN BOTH ARRAYS
-    dataProducer.filter(dataItem => filterArray.contains(dataItem))
+    array: Array[String] => array.filter(filterArray.contains(_))
   }
 
   val filterApplied = functionalComputation(filterData)
--- src/main/scala/lectures/operators/EvaluateOptimization.scala
+++ src/main/scala/lectures/operators/EvaluateOptimization.scala
@@ -1,6 +1,9 @@
 package lectures.operators
 
-import lectures.functions.Data
+import lectures.functions.Computation.{computation, dataArray, filterData}
+import lectures.functions.CurriedComputation.{dataArray, partiallyAppliedCurriedFunction}
+import lectures.functions.FunctionalComputation.{dataArray, filterApplied}
+import lectures.functions.{Computation, Data}
 
 /**
   * В задачке из lectures.functions.Computations мы реализовали
@@ -19,42 +22,42 @@ object EvaluateOptimization extends App with Data {
 
   val computationStartTimestamp = System.currentTimeMillis()
 
-//  // ВЫПОЛНИТЬ В ЦИКЛЕ ОТ 1 ДО 100 Computation.computation
-//  for (??? <- ???) {
-//    ???
-//  }
-//
-//  println("Elapsed time in computation(): " + (System.currentTimeMillis() - computationStartTimestamp))
-//
-//
-//
-//  val partiallyAppliedStartTimestamp = System.currentTimeMillis()
-//
-//  // ВЫПОЛНИТЬ В ЦИКЛЕ ОТ 1 ДО 100 CurriedComputation.partiallyAppliedCurriedFunction
-//  for (??? <- ???) {
-//    ???
-//  }
-//
-//  val partiallyAppliedDuration = System.currentTimeMillis() - partiallyAppliedStartTimestamp
-//  println("Elapsed time in partiallyAppliedCurriedFunction(): " + partiallyAppliedDuration)
-//
-//
-//
-//  val filterAppliedStartTimestamp = System.currentTimeMillis()
-//
-//  // ВЫПОЛНИТЬ В ЦИКЛЕ ОТ 1 ДО 100 FunctionalComputation.filterApplied
-//  for (??? <- ???) {
-//    ???
-//  }
-//
-//  val filterAppliedDuration = System.currentTimeMillis() - filterAppliedStartTimestamp
-//  println("Elapsed time in filterApplied():" + filterAppliedDuration)
-//
-//  // ВЫВЕСТИ РАЗНИЦУ В ПРОДОЛЖИТЕЛЬНОСТИ ВЫПОЛНЕНИЯ МЕЖДУ КАРРИРОВАННОЙ ВЕРСИЕЙ
-//  // И ФУНКЦИОНАЛЬНОЙ
-//
-//  val diff = ???
-//
-//  println(s"Difference is about $diff milliseconds")
+  // ВЫПОЛНИТЬ В ЦИКЛЕ ОТ 1 ДО 100 Computation.computation
+  for (_ <- 1 to 100) {
+    computation(filterData, dataArray)
+  }
+
+  println("Elapsed time in computation(): " + (System.currentTimeMillis() - computationStartTimestamp))
+
+
+
+  val partiallyAppliedStartTimestamp = System.currentTimeMillis()
+
+  // ВЫПОЛНИТЬ В ЦИКЛЕ ОТ 1 ДО 100 CurriedComputation.partiallyAppliedCurriedFunction
+  for (_ <- 1 to 100) {
+    partiallyAppliedCurriedFunction(dataArray)
+  }
+
+  val partiallyAppliedDuration = System.currentTimeMillis() - partiallyAppliedStartTimestamp
+  println("Elapsed time in partiallyAppliedCurriedFunction(): " + partiallyAppliedDuration)
+
+
+
+  val filterAppliedStartTimestamp = System.currentTimeMillis()
+
+  // ВЫПОЛНИТЬ В ЦИКЛЕ ОТ 1 ДО 100 FunctionalComputation.filterApplied
+  for (_ <- 1 to 100) {
+    filterApplied(dataArray)
+  }
+
+  val filterAppliedDuration = System.currentTimeMillis() - filterAppliedStartTimestamp
+  println("Elapsed time in filterApplied():" + filterAppliedDuration)
+
+  // ВЫВЕСТИ РАЗНИЦУ В ПРОДОЛЖИТЕЛЬНОСТИ ВЫПОЛНЕНИЯ МЕЖДУ КАРРИРОВАННОЙ ВЕРСИЕЙ
+  // И ФУНКЦИОНАЛЬНОЙ
+
+  val diff = partiallyAppliedDuration - filterAppliedDuration
+
+  println(s"Difference is about $diff milliseconds")
 }
 
